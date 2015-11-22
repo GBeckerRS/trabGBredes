c9c45d6a491a7cae5b61626388d49464209bb021
Gabriel Becker <gabrieloliveirabecker@gmail.com>
2015-11-22 11:49:34 -0200

    Adinado mensagens informativas

--- arquivo.py
+++ arquivo.py
@@ -6,9 +6,9 @@ class Arquivo:
         self.nome = nome
         self.estado = ''
         self.arq = None
+        print 'Criado o objeto Arquivo...'
 
     def abre (self, modo):
-        print 'Abrindo o arquivo'
         if (self.estado != ''):
             return -1 # Arquivo esta aberto
 
@@ -20,6 +20,7 @@ class Arquivo:
         except IOError as ex:
             print 'Handling error: ', ex
             raise
+        print 'Abriu o arquivo...'
 
         self.estado = 'LEITURA'
         return True
@@ -32,15 +33,20 @@ class Arquivo:
             return -11  # Arquivo nao esta em modo de leitura
 
         s = self.arq.read ()
+        print 'Leu dados do arquivo...'
 
         return s
 
-    def escreve (self):
+    def escreve (self, dados):
         print 'Escreve no arquivo'
         if (self.arq is None):
             return -1   # Arquivo nao esta aberto
         if (self.estado != 'ESCRITA'):
             return -20  # Arquivo nao esta em modo de escrita
+
+        self.arq.write (dados)
+        self.arq.flush ()
+        print 'Escreveu dados no arquivo...'
 
     def fecha (self):
         print 'fechando o arquivo'
@@ -54,4 +60,5 @@ class Arquivo:
             self.nome = ''
             self.estado = ''
             self.arq = None
+            print 'Fechou o arquivo...'
 
