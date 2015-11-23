9396e4a7db86747d289ff4400413c91601806fe1
Gabriel Becker <gabrieloliveirabecker@gmail.com>
2015-11-23 01:04:11 -0200

    Versão inicial da aplicação executada como cliente

---
+++ cliente.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env/python
+
+import sys
+
+from terminal import *
+from arquivo import *
+from socket_cliente import *
+
+class Cliente:
+    def __init__ (self, interfaceGrafica):
+        self.interfaceGrafica = interfaceGrafica
+        self.ip = ''
+        self.porta = 0
+        self.nomeArquivo = ""
+        self.term = None
+        self.arquivo = None
+        self.buffer = ''
+        print 'Construtor cliente'
+
+    def executa (self):
+        print 'Executa cliente'
+        dados = ''
+
+        # Inicializa atributos do cliente
+        self.inicializa ()
+
+        # Le dados do arquivo que sera enviado
+        self.arquivo = Arquivo (self.nomeArquivo)
+        self.arquivo.abre ('r')
+        dados = self.arquivo.le ()
+        self.arquivo.fecha ()
+
+        # Envia dados para o servidor
+        soc = Socket_cliente (self.ip, self.porta)
+        soc.enviaDados (dados)
+
+    def inicializa (self):
+        if (self.interfaceGrafica == 'N'):
+            # Interface grafica desabilitada
+            self.term = Terminal ()
+        else:
+            # Interface grafica habilitada (recurso indisponivel)
+            self.term = Terminal ()
+
+        # Requisita o ip do host
+        self.ip = self.term.leIp ()
+
+        # Requisita a porta do cliente
+        self.porta = self.term.lePorta ()
+
+        # Requisita o caminho do arquivo
+        self.nomeArquivo = self.term.leCaminhoArquivo ()
+
